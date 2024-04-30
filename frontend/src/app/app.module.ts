0309856ffe43bf70b24f2e4c41e2f37b9f12b359
nsu <nataniel.susantoputra@profect.de>
2024-04-30 18:04:31 +0200

    - deleted angular material & cdk - adjusted frontend (changed title)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,19 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatTableModule } from '@angular/material/table';
-import { MatTabsModule } from '@angular/material/tabs'; 
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button'; 
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatRadioModule } from '@angular/material/radio';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -28,9 +17,6 @@ import { LoginService } from './services/login.service';
 import { NeightWeddingPagenotfoundComponent } from './component/neight-wedding-pagenotfound/neight-wedding-pagenotfound.component';
 import { FetchGuestService } from './services/fetch-guest.service';
 import { NeightWeddingQrCodeComponent } from './component/neight-wedding-qr-code/neight-wedding-qr-code.component';
-import { NeightToastTemplateComponent } from './component/neight-toast-template/neight-toast-template.component';
-import { ToastService } from './services/toast.service';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 
@@ -40,20 +26,8 @@ import { EffectsModule } from '@ngrx/effects';
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
-        MatDialogModule,
-        MatIconModule,
-        MatMenuModule,
-        MatSlideToggleModule,
-        MatTableModule,
         FormsModule,
         ReactiveFormsModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatButtonModule,
-        MatButtonToggleModule,
-        MatRadioModule,
-        MatTabsModule,
-        MatSnackBarModule,
         // StoreModule.forRoot({
         //   neightWeddingGuestList: neightWeddingGuestListReducer,
         //   neightWeddingGuestFamilyList: neightWeddingGuestFamilyListReducer,
@@ -67,13 +41,11 @@ import { EffectsModule } from '@ngrx/effects';
         AppComponent,
         NeightWeddingPagenotfoundComponent,
         NeightWeddingQrCodeComponent,
-        NeightToastTemplateComponent,
     ],
     providers: [
         NeightApiService,
         LoginService,
         FetchGuestService,
-        ToastService,
         { provide: NEIGHT_CONFIG, useValue: neightEnvironment },
     ],
     bootstrap: [AppComponent]
