441ae04a14b9aebdc63d14676012c207f64064d7
nsu <nataniel.susantoputra@profect.de>
2024-05-10 01:33:58 +0200

    - menu items as dict

--- frontend/src/app/component/neight-tech-wedding-home/neight-tech-wedding-home.component.ts
+++ frontend/src/app/component/neight-tech-wedding-home/neight-tech-wedding-home.component.ts
@@ -8,4 +8,17 @@ import { Component } from '@angular/core';
 export class NeightTechWeddingHomeComponent {
 
   theme = 'g90';
+
+  menuItems: any = [
+    {
+      title: 'Dashboard',
+      icon: 'template',
+      size: '16'
+    },
+    {
+      title: 'Guest Traffic',
+      icon: 'activity',
+      size: '16'
+    },
+  ]
 }
